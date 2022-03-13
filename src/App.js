e1797b8d03dd7f2c3ef497467af0d92fe6489140
gilhan <rlfgks97@gmail.com>
2022-03-13 22:14:29 +0900

    #6.4 - 2nd
    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import { useState, useEffect } from "react";
 
 function Hello() {
-  function byeFn() {
-    console.log("destroyed :(");
-  }
-  function hiFn() {
-    console.log("created :(");
-    return byeFn;
-  }
-  useEffect(hiFn, []);
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
   return <h1>Hello</h1>;
 }
 
